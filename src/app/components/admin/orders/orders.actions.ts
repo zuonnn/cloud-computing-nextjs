3ecc8007c44e40371aff547f87e54663bf9631f2
Tran Ngoc Duong <tr.ngocduong01@gmail.com>
2023-12-15 10:44:16 +0700

    fix order

--- src/app/components/admin/orders/orders.actions.ts
+++ src/app/components/admin/orders/orders.actions.ts
@@ -1,6 +1,4 @@
 'use server'
-import { revalidateTag } from 'next/cache'
-import { IOrder } from '../../types/order'
 
 export const handleGetOrders = async () => {
     const res = await fetch(`https://duong211404.onrender.com/orders`,
@@ -10,55 +8,4 @@ export const handleGetOrders = async () => {
         });
     const data = await res.json();
     return Response.json({data});   
-}
-
-export async function handleCreateOrderAction(orderData: IOrder): Promise<boolean> {
-    try {
-      const response = await fetch(`https://duong211404.onrender.com/orders`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(orderData),
-      });
-  
-      if (response.ok) {
-        // Order created successfully
-        return true;
-      } else {
-        // Order creation failed
-        const errorData = await response.json();
-        console.error('Failed to create order:', errorData);
-        return false;
-      }
-    } catch (error) {
-      console.error('Error creating order:', error);
-      return false;
-    }
-  }
-  
-
-export const handleUpdateOrderAction = async (data: IOrder) => {
-    const res = await fetch(`https://duong211404.onrender.com/orders/${data._id}`, {
-        method: "PUT",
-        body: JSON.stringify(data),
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    });
-    revalidateTag("list-orders");
-    return await res.json();
-}
-
-export const handleDeleteOrderAction = async (id: string) => {
-    const res = await fetch(`https://duong211404.onrender.com/orders/${id}`, {
-        method: "DELETE",
-        headers: {
-            "Content-Type": "application/json",
-            // 'Content-Type': 'application/x-www-form-urlencoded',
-        },
-    });
-    revalidateTag("list-orders");
-    return await res.json();
 }
